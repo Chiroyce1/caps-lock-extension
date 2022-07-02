fef2402ce51e32deba587629d830e4154ef876fc
JaydenDev <willsjayden2@gmail.com>
2022-07-02 13:35:46 -0500

    feat: improve design

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const icon = document.createElement('span');
-icon.style.backgroundColor = "black";
-icon.style.color = "red";
-icon.style.padding = "0.5em";
-icon.style.borderRadius = "3px";
+icon.style.backgroundColor = "#121212";
+icon.style.color = "#FFCC00";
+icon.style.padding = "0.8em";
+icon.style.borderRadius = "5px";
 icon.style.zIndex = 9999999999;
 icon.innerText = "Don't forget to turn off caps lock!";
-
+icon.style.fontFamily = "Noto Sans, sans-serif";
 icon.style.position = "fixed";
 icon.style.top = "0";
 icon.style.left = "0";
